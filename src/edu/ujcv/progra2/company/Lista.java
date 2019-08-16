ee31bda733c6edca4804618d6547c2ba55f04830
orlinelvir <orlin.elvir@gmail.com>
2019-08-16 07:36:24 -0600

    Clase para que el menú expulse la lista de productos y clientes que creamos en el programa

---
+++ src/edu/ujcv/progra2/company/Lista.java
@@ -0,0 +1,11 @@
+package edu.ujcv.progra2.company;
+
+public interface Lista <T>{
+    public void add(T data);
+    public boolean isEmpty();
+    public T remove(int index);
+    public int getSize();
+    public T getItem(int index);
+
+
+}
